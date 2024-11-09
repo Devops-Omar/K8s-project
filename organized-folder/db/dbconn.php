6c8a48e78b50e25127cfb251e2da1bcba6441bb3
Omar <168889108+Devops-Omar@users.noreply.github.com>
2024-11-09 21:45:46 +0100

    Delete organized-folder directory

--- organized-folder/db/dbconn.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conn = mysqli_connect("mysql-svc", "root", "myrootpassword", "omartill");
-
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-// Adding a test comment to ensure file is detected as changed
-?>
